cf7d6862818a853dae8f5671f2b0cde721629ef1
theTeacher <thTeacher@et.com>
2020-06-15 19:35:22 +0800

    老师的第一次提交

---
+++ Teacher.java
@@ -0,0 +1,12 @@
+/*
+* @Author: Marte
+* @Date:   2020-06-15 19:31:00
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-06-15 19:31:42
+*/
+
+public class java {
+    public static void main(String[] args) {
+
+    }
+}
